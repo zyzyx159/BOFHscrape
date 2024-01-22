8cd57bf27e5ccfce3ef081c67c3539d609248465
Daniel Stellmon <Dstellm@gmail.com>
2024-01-22 02:51:17 -0600

    fixed write blank DB.YAML

--- bin/index.js
+++ bin/index.js
@@ -2,21 +2,33 @@ import * as utils from './utils.js';
 import * as linkScrape from './linkScrape.js';
 import * as storyScrape from './storyScrape.js';
 import * as test from './test.js'
+import * as fs from 'fs';
 
-const URL = "https://www.theregister.com/offbeat/bofh/"
-const path = './DB.yaml'
+const currentUrl = "https://www.theregister.com/offbeat/bofh/"
+const earlierUrl = "https://www.theregister.com/offbeat/bofh/earlier/" // add a "#/" for each following page
+const pathToDB = './DB.yaml'
 
-if !fs.existsSync(path){
+if (!(fs.existsSync(pathToDB))){
   fs.writeFile('./DB.yaml')
 }
 
-linkScrape.getLinks(URL).then(onLinkScrapeSucess, onLinkScrapError)
+run(currentUrl);
+//TODO: Eventually this needs to check if we need the archive function 
 
-function onLinkScrapError{
+function run(Url){
+  linkScrape.getLinks(URL).then(onLinkScrapeSucess, onLinkScrapError)
+}
+
+function onLinkScrapError(){
   console.log("onLinkScrapError: No links found.")
 }
 
-function onLinkScrapeSucess(links)
-    
+function onLinkScrapeSucess(links){
+   var downLoadLinks = DBUtils.filterUrls(links, pathToDB);
+  
+  for(let i = 0; i < downLoadLinks.legenth; i++){
+    console.log(downLoadLinks[i]);
+  }
+
 }
 
