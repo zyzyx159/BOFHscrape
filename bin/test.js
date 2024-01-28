825d73f8dd92e44c2454174caeb0a81251e1f7a8
Daniel Stellmon <Dstellm@gmail.com>
2024-01-28 05:31:19 -0600

    local or WWW scrape test

--- bin/test.js
+++ bin/test.js
@@ -1,20 +1,29 @@
 import * as linkScrape from './linkScrape.js';
+import * as localLinkScrape from './localLinkScrape.js';
+import * as dbUtils from './DBUtils.js';
 
-//const URL = "https://www.theregister.com/offbeat/bofh/";
-const URL = "./BOFH.html"
+const URL = "https://www.theregister.com/offbeat/bofh/";
+const localURL = "./BOFH.html";
+const dbPath = "./DB.yaml";
 
 function onSucess(links) {
-  for(let i = 0; i < links.legenth; i++){
+/*   for(let i = 0; i < links.legenth; i++){
     console.log(links[i]);
-  }
+  } */
+  dbUtils.filterUrls(links, dbPath);
 }
 
 function onError() {
   console.log("no links found")
 }
 
-export function testLinkScrape(URL){
-  localLinkScrape.getLinks(URL).then(onSucess, onError)
+function testLinkScrape(URL){
+  linkScrape.getLinks(URL).then(onSucess, onError)
+}
+
+function testLocalLinkScrape(localURL){
+  localLinkScrape.getLinks(localURL).then(onSucess, onError)
 }
 
 testLinkScrape(URL);
+testLocalLinkScrape(localURL);
