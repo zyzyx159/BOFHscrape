e73a072bb927e89fd3cf007feb40e986512008f2
Daniel Stellmon <Dstellm@gmail.com>
2024-01-28 04:50:52 -0600

    added files for local testing

---
+++ bin/localLinkScrape.js
@@ -0,0 +1,33 @@
+//ACCEPT: web address 
+//RETURN: list of BOFH links
+
+import * as fs from 'fs';
+import * as cheerio from 'cheerio';
+
+export function getLinks(URL) {
+  return new Promise(function(resolve, reject){
+    fs.readFile("./BOFH.html").then(response => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const links = [];
+
+      $('.story_link').each( function (){
+        var link = $(this).attr('href');
+        if (link.includes("bofh")){
+          links.push("https://www.theregister.com" + link);
+        }
+      });
+
+      //if links.count > 0 resolve else reject
+      
+      if (links.length > 0) {
+        for(let i = 0; i < links.length; i++){
+          console.log(links[i]);
+        }
+        resolve(links);
+      } else {
+        reject('No links found');
+      }
+    })
+  })
+}
