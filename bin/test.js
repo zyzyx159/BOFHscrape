61165e43ea61e6abb9f716ca6d13d78abc29acf3
Daniel Stellmon <Dstellm@gmail.com>
2024-02-05 05:05:11 -0600

    I updated testing code

--- bin/test.js
+++ bin/test.js
@@ -1,29 +1,10 @@
 import * as linkScrape from "./linkScrape.js";
 import * as localLinkScrape from "./localLinkScrape.js";
 import * as dbUtils from "./DBUtils.js";
+import * as scrape from "./scraper.js";
 
 const URL = "https://www.theregister.com/offbeat/bofh/";
 const localURL = "./BOFH.html";
 const dbPath = "./DB.yaml";
 
-function onSucess(links) {
-  /*   for(let i = 0; i < links.legenth; i++){
-    console.log(links[i]);
-  } */
-  dbUtils.filterUrls(links, dbPath);
-}
-
-function onError() {
-  console.log("no links found");
-}
-
-function testLinkScrape(URL) {
-  linkScrape.getLinks(URL).then(onSucess, onError);
-}
-
-function testLocalLinkScrape(localURL) {
-  localLinkScrape.getLinks(localURL).then(onSucess, onError);
-}
-
-testLinkScrape(URL);
-testLocalLinkScrape(localURL);
+scrape.localHTML(localURL).then(scrape.filterLinks);
