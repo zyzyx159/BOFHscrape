c5a42c90ca84b3af58d883b83960e70198206122
Daniel Stellmon <Dstellm@gmail.com>
2024-02-12 05:04:21 -0600

    working print links example

--- test/test.js
+++ test/test.js
@@ -4,24 +4,15 @@ import * as filterHTML from "../bin/filterHTML.js";
 import * as output from "../bin/output.js";
 
 const URL = "https://www.theregister.com/offbeat/bofh/";
-const localURL = "./test/BOFH.html";
+const localURL = "../test/BOFH.html";
 const dbPath = "../DB.yaml";
 const localStory = "./test/BOFH_Episode.html";
 
-// getHTML.localHTML(localURL).then(
-//   //add the database filter in here
-//   filterHTML.filterLinks
-// );
+//print list of links
+getHTML.localHTML(localURL).then(filterHTML.filterLinks).then(printArray);
 
-function getHTML2() {
-  let bob = new map(
-    (bob = getHTML.localHTML(localStory).then(filterHTML.filterStories))
-  );
-  return bob;
+function printArray(data) {
+  for (let i = 0; i < data.length; i++) {
+    console.log(data[i]);
+  }
 }
-
-//output.writeStory(HTML, URL);
-
-let bob = getHTML.localHTML(localStory).then(filterHTML.filterStories);
-console.log(bob.pubdate);
-console.log("I'm a failure");
