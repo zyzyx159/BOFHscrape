dc340ebbe219ff060274f5276ce493bc755389b5
Daniel Stellmon <Dstellm@gmail.com>
2024-02-25 03:50:11 -0600

    make methods private and move in cherio

--- bin/story.js
+++ bin/story.js
@@ -1,9 +1,15 @@
-class story{
-    constructor(URL){
-        this.setURL(URL);
-    }
+import * as cheerio from "cheerio";
+import * as axios from "axios";
 
-    setURL(newURL){
+class story{
+    //#region - constructor
+    constructor(newURL){
+        this.#setURL(newURL);
+    }
+    //#endregion
+
+    //#region - Getters and Setters
+    #setURL(newURL){
         if (newURL === '') {
             throw 'The URL cannot be empty';
         }
@@ -13,42 +19,53 @@ class story{
         return this.URL;
     }
 
-    setTitle(newTitle){
+    #setHTML(){
+        return new Promise(function (resolve, reject) {
+            axios.get(this.#URL).then((response) => {
+              resolve(response.data);
+            });
+          });
+    }
+    getHTML(){
+        return this.HTML;
+    }
+
+    #setTitle(newTitle){
         this.title = newTitle.trim();
     }
     getTitle(){
         return this.title;
     }
 
-    setSubtitle(newSubtitle){
+    #setSubtitle(newSubtitle){
         this.subtitle = newSubtitle.trim();
     }
     getSubtitle(){
         return this.subtitle;
     }
 
-    setAuthor(newAuthor){
+    #setAuthor(newAuthor){
         this.Author = newAuthor.replace(/\r?\n|\r/g, " ").trim();
     }
     getAuthor(){
         return this.Author;
     }
 
-    setEpisode(newEpisode){
+    #setEpisode(newEpisode){
         this.Episode = newEpisode.trim();
     }
     getEpisode(){
         return this.Episode;
     }
 
-    setPublishDate(newPublishDate){
+    #setPublishDate(newPublishDate){
         this.PublishDate = newPublishDate.replace(/\s\s+/g, ' ');
     }
     getPublishDate(){
         return this.PublishDate;
     }
 
-    SetStory(newStoryArray){
+    #SetStory(newStoryArray){
         for (i = 0; i < newStoryArray.length; i++){
             newStory = newStory + newStoryArray[i] + "/n"
         }
@@ -57,4 +74,29 @@ class story{
     getStory(){
         return this.story;
     }
+    //#endregion
+
+    //#region - Cherio: process HTML
+    processHTML(){
+        $ = cheerio.load(this.html);
+        episodeElements = $("div[id=page] > article");
+
+        this.setTitle(episodeElements.find("div[class=header_right] > h1").text());
+  
+        this.setSubtitle(episodeElements.find("div[class=header_right] > h2").text());
+  
+        this.setEpisode(episodeElements.find("#body > p:nth-child(1) > span").text());
+  
+        this.setAuthor(episodeElements.find("a.byline").text());
+  
+        this.setPublishDate(episodeElements.find("span[class=dateline]").text());
+  
+        this.setStory(
+          episodeElements
+            .find("div[id=body] > p")
+            .toArray()
+            .map((element) => $(element).text())
+        );
+    }
+    //#endregion
 }
